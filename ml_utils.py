95aa0d38ebe7be64f0d9937e176fed57a8f7b396
allyryry <aryan12@nait.ca>
2022-01-13 19:39:17 -0700

    a

--- ml_utils.py
+++ ml_utils.py
@@ -158,3 +158,41 @@ class edaDF:
         sns.distplot(df2[columnName], color="green")
         plt.show()
         return
+
+    
+    def CohenEffectSize(group1, group2):
+        """Computes Cohen's effect size for two groups.
+        
+        group1: Series or DataFrame
+        group2: Series or DataFrame
+        
+        returns: float if the arguments are Series;
+                Series if the arguments are DataFrames
+        """
+        diff = group1.mean() - group2.mean()
+
+        var1 = group1.var()
+        var2 = group2.var()
+        n1, n2 = len(group1), len(group2)
+
+        pooled_var = (n1 * var1 + n2 * var2) / (n1 + n2)
+        d = diff / np.sqrt(pooled_var)
+        return d
+
+#THINKSTATS 2
+
+ def mean(self):
+        """Computes the mean of a PMF.
+
+        Returns:
+            float mean
+        """
+        return sum(p * x for x, p in self.Items())
+
+ def Median(self):
+        """Computes the median of a PMF.
+
+        Returns:
+            float median
+        """
+        return self.MakeCdf().Percentile(50)
